af6867b1cd34a2886921d0cd14413cf7993b761e
unknown <raymondlevengood@gmail.com>
2019-12-10 14:48:09 -0500

    added more precise psuedocode. still working on loading questions the field

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-console.log("Let's Begin!");
+// console.log("Let's Begin!");
 
 //https://opentdb.com/api_config.php
 //Open Trivia Database API
@@ -8,8 +8,6 @@ let scoreNum = document.querySelector("#score");
 let ruleSet = document.querySelector("#rules");
 let timerNum = document.querySelector("#timer");
 let startGameButton = document.querySelector("#startGame");
-let quizList = [];
-// let newArray = [];
 
 let questions = document.querySelector("#question");
 let answerOne = document.querySelector("#answer1");
@@ -30,7 +28,7 @@ function grabQuiz() {
   fetch(quizGrab)
     .then(res => res.json())
     .then(quizData => {
-      console.log(quizData);
+      // console.log(quizData);
       makeQuiz(quizData);
     })
     .catch(err =>
@@ -38,31 +36,55 @@ function grabQuiz() {
     );
 }
 
+let questionList = [];
+
 function makeQuiz(quizData) {
   for (i = 0; i < quizData.results.length; i++) {
-    let question1 = quizData.results[0]
-    quizList.push(quizData.results[i]);
-    // quizList[i].newArray = [];
-    // quziList[i].newArray.push(quizData.results[i].correct_answer);
-    // for (i = 0; i < quizData.reuslts[i].incorrect_answerl; i++) {
-    //   quziList[i].newArray.push(quizData.results[i].incorrect_answers);
+    var quizList = quizData.results[i];
+    questionList.push(quizList);
+    questionList[i].answerList = [];
+    console.log(quizList)
+    questionList[i].answerList.push(quizList.correct_answer);
+    console.log(quizList.incorrect_answers)
+    // for (i = 0; i < quizList.incorrect_answers.length; i++) {
+    //   questionList[i].answerList.push(quizList.incorrect_answers[i].length); // THIS NEEDS SOMETHING MORE EYES PLS
     // }
   }
-//   console.log(newArray);
-  //somewhere math.random
-  //apply quiestio to bock
-  //apply anwsers to buttons
-  //possibly something else start timer
-  //end
-  console.log(quizList);
+  // var correctAnwser = quizList.corrext_answer
+  // answerList.sort(math.random())
+  //questions.innerHTML = questionList[0].question
+  //answerOne.innerHTML = questionList[0].answerList[0]
+  //answerTwo.innerHTML = questionList[0].answerList[1]
+  //answerThree.innerHTML = questionList[0].answerList[2]
+  //answerFour.innerHTML = questionList[0].answerList[3]
+  //timer()
 }
 
 // function timer() {
-
+  //let timeLeft = 30
+  //for(i = 30; i >= 0; i--) {
+  //  setTimeout(tick, 1000)
+  //  timeLeft = i
+  //  if(i = 0){
+  //    alert('Time's Up!)
+  //  }
+  //} 
 // }
 
 function checkAnswer() {
   console.log("checking...");
+  //if ( 'userInput' = correctAnswer) {
+  //  alert('Correct!')
+  //  // change score here
+  //      if (question = 10) {
+  //          alert('You got ' + scoreNum + '.')
+  //      } else {move on to next question}
+  //} else {
+  //    alert('Incorrect')
+  //      if (question = 10) {
+  //          alert('You got ' + scoreNum + '.')
+  //      } {move on to next question}
+  //}
   //checks
   // if correct_anwser is chosen +1 to score
   // else move to next question
@@ -71,4 +93,5 @@ function checkAnswer() {
   // if quiestion = 10
   // end
 }
-// makeQuiz()
+
+// console.log(quizList);
